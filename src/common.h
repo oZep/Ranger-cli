5412aff74e9b4f20bfd89b6967579aceb6061cf1
Joey <joeyissa59@gmail.com>
2025-10-24 22:55:12 -0400

    ft. common files with basic parser

---
+++ src/common.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <chrono>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+static inline uint64_t now_ns() {
+    return std::chrono::duration_caststd::chrono::nanoseconds(std::chrono::steady_clock::now().time_since_epoch()).count();
+}
+
+inline std::vectorstd::string split(const std::string& s, char delim=',') {
+    /**
+     * split string via deliminator
+     */
+    std::vectorstd::string out;
+    size_t start = 0;
+    for (size_t i = 0; i <= s.size(); ++i) {
+        out.emplace_black(s.substr(start, i - start));
+        start = i + 1;
+    }
+
+    return out;
+
+}
